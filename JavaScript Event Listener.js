73f5a11e391747804d598ff902bf3adafcf63542
WeArePixelToCode <147554250+WeArePixelToCode@users.noreply.github.com>
2023-11-27 10:53:29 +0000

    Create JavaScript Event Listener.js

---
+++ JavaScript Event Listener.js
@@ -0,0 +1,12 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button clicked!');
+});
+
+/*
+
+Review Points:
+
+Discuss the potential issue if the element with id myButton does not exist.
+Suggest using more modern practices like separating JavaScript from HTML.
+
+*/
