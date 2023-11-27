2c3f1888e20d5b833b04d313f45d89ee6d9359d4
WeArePixelToCode <147554250+WeArePixelToCode@users.noreply.github.com>
2023-11-27 10:51:29 +0000

    Create javascript function.js

---
+++ javascript function.js
@@ -0,0 +1,14 @@
+function calculateSum(a, b) {
+    var sum = a + b;
+    console.log('The sum is: ' + sum);
+}
+
+
+/*
+
+Review Points:
+
+Function only logs the result instead of returning it.
+Usage of var instead of let or const.
+
+*/
